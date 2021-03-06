e343faef6dd34607a4b54b4564650980cae0f32b
Menamonmon <monyasser111@gmail.com>
2021-03-06 15:34:11 -0500

    added routes component

---
+++ frontend/src/components/Routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import { HomePage, AboutPage, ProjectsPage, NotFoundPage } from "../pages";
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route exact strict path="/" component={HomePage} />
+      <Route exact strict path="/about" component={AboutPage} />
+      <Route exact strict path="/projects" component={ProjectsPage} />
+      <Route exact strict path="*" component={NotFoundPage} />
+    </Switch>
+  );
+};
+
+export default Routes;
