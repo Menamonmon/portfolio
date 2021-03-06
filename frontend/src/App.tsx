080f4330275c8ad57c2eee7271df4175338de76f
Menamonmon <monyasser111@gmail.com>
2021-03-06 13:28:13 -0500

    added navbar to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+
+import Navbar from "./components/Navbar";
+
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar
+        title="Mena Filfil"
+        homeLink={{ name: "Home", path: "/", key: "home" }}
+        links={[
+          { name: "Projects", path: "/projects", key: "projects" },
+          { name: "About", path: "/about", key: "projects" },
+        ]}
+      ></Navbar>
+    </div>
+  );
 };
 
 export default App;
